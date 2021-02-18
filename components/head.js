9c9789e0a3c63de280ceaf5f913a235e4ce1aea5
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-02-17 20:43:17 -0600

    🎉 Begin template

---
+++ components/head.js
@@ -0,0 +1,10 @@
+import { Head as NextHead } from 'next/head'
+
+export default function Head() {
+    return (
+        <NextHead>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        </NextHead>
+    )
+}
---
+++ utils/parser.js
@@ -0,0 +1,1 @@
+// first emoji parse, then markdown parse
